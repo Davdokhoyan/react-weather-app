2224ff84df48e7d86dca9d8f7378f805c7c65456
David <davdokhoyan@mail.ru>
2023-02-22 17:08:17 +0400

    react commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7b5c9f9c75msh3f3ae9d03a2e29fp169abfjsnfaa300360a37",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "41f68b89e0d9bd3197444f039756d0aa";
